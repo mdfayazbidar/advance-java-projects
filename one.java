6586d79a38a74e9382ffc13899baad532aa41ec5
mdfayazbidar <125641962+mdfayazbidar@users.noreply.github.com>
2023-02-17 13:54:40 +0530

    Create one.java
    done

---
+++ one.java
@@ -0,0 +1,26 @@
+package corejava;
+//Greatest among three numbers
+import java.util.Scanner;
+public class one {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter 3 numbers");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		if(a>b && a>c)
+		System.out.println("a is greatest");
+		else if(b>c&&b>a) 
+				System.out.println("b is greatest");
+			else
+				System.out.println("c is greatest");
+
+	
+				
+		// TODO Auto-generated method stub
+
+	}
+
+}
