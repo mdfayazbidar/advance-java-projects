7cfa6e7d2eb051cf209a0b4e0b28a25f2fdf755f
mdfayazbidar <125641962+mdfayazbidar@users.noreply.github.com>
2023-02-27 10:34:58 +0530

    Add files via upload

--- SingleThreadTest.java
+++ SingleThreadTest.java
@@ -4,12 +4,15 @@ class MyThread extends Thread{
 String name;	
 MyThread(String threadname){
 	name=threadname;
-	System.out.println("one"+"thread created");
+	System.out.println(name+"thread created");
 }
 public void run() {
+	try {
 	for(int i=1;i<=5; i++) {
-System.out.println(i);
+System.out.println(name+""+i);
+Thread.sleep(1000);
 }
+}catch(Exception e) {}
 }
 }
 public class SingleThreadTest {
@@ -19,7 +22,15 @@ public class SingleThreadTest {
 		System.out.println("hello");
 		// context switch
 		MyThread m1=new MyThread("one");
+		MyThread m2=new MyThread("two");
+		MyThread m3=new MyThread("thee");
+		MyThread m4=new MyThread("four");
+		MyThread m5=new MyThread("fifth");
 		m1.start();
+		m2.start();
+		m3.start();
+		m4.start();
+		m5.start();
 		Thread.sleep(5000);
 		System.out.println("bye");
 
